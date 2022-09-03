0630d17611f52694df02f8b763a32ee617715830
Arbaz86 <arbaz10042002@gmail.com>
2022-09-03 10:04:47 +0530

    did credit part

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -30,9 +30,9 @@ const Feed = () => {
         <Typography
           className="copyright"
           variant="body2"
-          sx={{ mt: 1.5, color: "#fff" }}
+          sx={{ mt: 1.5, color: "#fff", fontSize: "12px" }}
         >
-          Copyright 2022 JSM Media
+          Credit goes to JSM Media
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
